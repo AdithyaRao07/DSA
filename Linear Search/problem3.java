7879ac595a6271fa88f5a9b68a2b790ac4090b4a
Adithya <adithyamadupu07@gmail.com>
2025-09-11 21:27:59 +0530

    Added practice problems on 2D arrays searching

--- Linear Search/problem3.java
+++ Linear Search/problem3.java
@@ -2,7 +2,9 @@ public class problem3 {
     public static void main(String[] args) {
     int[] arr={10,20,30,25,35};
         System.out.println(linearSearch(arr,1,4,30));
+        System.out.println(minsearch(arr));
     }
+    // Searching with in a Range
     public static boolean linearSearch(int[] arr,int start,int end,int target){
         for(int i=start;i<=end;i++){
             if(target==arr[i]){
@@ -10,5 +12,18 @@ public class problem3 {
             }
         }
         return  false;
+    }
+    // Searching minimum number in array
+    public static int minsearch(int[] arr){
+        if(arr.length==0){
+            return -1;
+        }
+        int min=arr[0];
+        for(int ele:arr){
+            if(ele<min){
+                min=ele;
+            }
+        }
+        return min;
     }
 }
